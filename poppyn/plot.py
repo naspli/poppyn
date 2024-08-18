1d27265bff844f58afba123711e0f76b9baf7561
luke <luke@naspli.tech>
2024-08-18 20:45:03 +0100

    [New Plots] higher res plots

--- poppyn/plot.py
+++ poppyn/plot.py
@@ -51,7 +51,7 @@ def plot_data(arr, slice_name=None, pop_target=None, scale=None, max_in=None, hi
             'family': 'sans-serif',  # Font family (e.g., 'serif', 'sans-serif', 'monospace')
             'style': 'normal',  # Font style ('normal', 'italic', 'oblique')
             'weight': 'bold',  # Font weight ('normal', 'bold', 'light', 'heavy', 'ultrabold')
-            'size': 16,  # Font size
+            'size': 24,  # Font size
             'color': 'white',  # Text color,
             'alpha': 0.4
         }
@@ -64,7 +64,7 @@ def plot_data(arr, slice_name=None, pop_target=None, scale=None, max_in=None, hi
                 f"  - In more dense areas, the population has been spread out;\n"
                 f"  - In less dense areas, the population has been grouped together. See code for details.\n"
                 f"On this {width}x{height} image, a pixel is around {int(round_sig_fig(scale, 2))}km$^2$.")
-        ax.text(16, height - 40, text, fontdict=font_properties, ha='left', va='top')
+        ax.text(16, height - 56, text, fontdict=font_properties, ha='left', va='top')
 
         font_properties = {**font_properties, 'size': 10}
         text = (f"Dataset: WorldPop - DOI 10.5258/SOTON/WP00647\n"
